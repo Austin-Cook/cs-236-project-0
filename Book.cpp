93c760c85605248c5d9b62993731da23fa88531c
Austin-Cook <austin.cook.id@gmail.com>
2022-09-02 11:31:21 -0600

    Fix bug with genreSet bool

--- Book.cpp
+++ Book.cpp
@@ -16,6 +16,7 @@ Book::Book(std::string title, std::string author) {
 
 void Book::setGenre(std::string genre) {
     this->genre = genre;
+	genreSet = true;	//This line didn't exist so hasGenre() always returned that the genre wasn't set previously
 }
 
 void Book::setPages(std::string pages) {
